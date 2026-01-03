43a18b35538c2f0f7ff6d89a8c46e97cc48ad118
david <mail@nonswag.dev>
2026-01-03 11:35:43 +0100

    Update build for Java 21, add publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,79 @@
 plugins {
     id("java")
+    id("java-library")
+    id("maven-publish")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.compileJava {
+    options.release.set(21)
 }
 
 group = "net.thenextlvl"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:6.1.0-SNAPSHOT"))
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.addAll(listOf("--add-reads", "net.thenextlvl.interfaces=ALL-UNNAMED"))
+}
+
+tasks.withType<Test>().configureEach {
+    jvmArgs("--add-reads", "net.thenextlvl.interfaces=ALL-UNNAMED")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("--add-reads", "net.thenextlvl.interfaces=ALL-UNNAMED")
+}
+
+tasks.withType<Javadoc>().configureEach {
+    val options = options as StandardJavadocDocletOptions
+    options.tags("apiNote:a:API Note:", "implSpec:a:Implementation Requirements:")
+    options.addStringOption("-add-reads", "net.thenextlvl.interfaces=ALL-UNNAMED")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showCauses = true
+        showExceptions = true
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = "interfaces"
+        groupId = "net.thenextlvl"
+        pom.scm {
+            val repository = "TheNextLvl-net/interfaces"
+            url.set("https://github.com/$repository")
+            connection.set("scm:git:git://github.com/$repository.git")
+            developerConnection.set("scm:git:ssh://github.com/$repository.git")
+        }
+        from(components["java"])
+    }
+    repositories.maven {
+        val branch = if (version.toString().contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$branch")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
 }
