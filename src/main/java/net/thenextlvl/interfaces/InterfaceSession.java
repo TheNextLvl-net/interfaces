d499e6e74cefe4eea36579bf77d668b4e47e7bea
david <mail@nonswag.dev>
2026-03-13 18:35:06 +0100

    Add state computation methods to InterfaceSession

--- src/main/java/net/thenextlvl/interfaces/InterfaceSession.java
+++ src/main/java/net/thenextlvl/interfaces/InterfaceSession.java
@@ -3,6 +3,11 @@ package net.thenextlvl.interfaces;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public sealed interface InterfaceSession permits RenderContext, SimpleInterface.Session {
     Interface getInterface();
@@ -16,7 +21,38 @@ public sealed interface InterfaceSession permits RenderContext, SimpleInterface.
     @Contract(pure = true)
     <T> T state(String key, Class<T> type, T fallback);
 
-    void state(String key, Object value);
+    /**
+     * @since 0.3.0
+     */
+    @Contract(pure = true)
+    <T> Optional<T> state(String key, Class<T> type);
+
+    @Contract(mutates = "this")
+    void state(String key, @Nullable Object value);
+
+    /**
+     * @since 0.3.0
+     */
+    @Contract(mutates = "this")
+    <T> @Nullable T computeState(String key, BiFunction<String, @Nullable T, @Nullable T> remappingFunction);
+
+    /**
+     * @since 0.3.0
+     */
+    @Contract(mutates = "this")
+    <T> @Nullable T computeStateIfAbsent(String key, Function<String, @Nullable T> remappingFunction);
+
+    /**
+     * @since 0.3.0
+     */
+    @Contract(mutates = "this")
+    <T> @Nullable T computeStateIfPresent(String key, BiFunction<String, @Nullable T, @Nullable T> remappingFunction);
+
+    /**
+     * @since 0.3.0
+     */
+    @Contract(pure = true)
+    boolean hasState(String key);
 
     void refresh();
 
